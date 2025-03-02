66096f824af04d97bc3a59aa60486b56e31d50b0
PHPJava666 <38599069+PHPJava666@users.noreply.github.com>
2025-03-02 11:18:39 +0800

    Update net_factory.py

--- code/networks/net_factory.py
+++ code/networks/net_factory.py
@@ -1,14 +1,14 @@
 from networks.unet import UNet_HL
-from networks.VNet import VNet
+from networks.VNet import VNet_HL
 import torch.nn as nn
 
 def net_factory(net_type="unet", in_chns=1, class_num=2, mode = "train", tsne=0):
     if net_type == "unet" and mode == "train":
         net = UNet_HL(in_chns=in_chns, class_num=class_num).cuda()
-    if net_type == "VNet" and mode == "train" and tsne==0:
-        net = VNet(n_channels=in_chns, n_classes=class_num, normalization='batchnorm', has_dropout=True).cuda()
-    if net_type == "VNet" and mode == "test" and tsne==0:
-        net = VNet(n_channels=in_chns, n_classes=class_num, normalization='batchnorm', has_dropout=False).cuda()
+    if net_type == "VNet_HL" and mode == "train" and tsne==0:
+        net = VNet_HL(n_channels=in_chns, n_classes=class_num, normalization='batchnorm', has_dropout=True).cuda()
+    if net_type == "VNet_HL" and mode == "test" and tsne==0:
+        net = VNet_HL(n_channels=in_chns, n_classes=class_num, normalization='batchnorm', has_dropout=False).cuda()
     return net
 
 
